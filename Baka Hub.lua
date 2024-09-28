0087d4da7c534d32ba07ef0c3ec92163eebb610d
mijikaisys <mikifemboy@gmail.com>
2024-09-28 09:38:00 +0200

    Update Baka Hub.lua

--- Baka Hub.lua
+++ Baka Hub.lua
@@ -42,20 +42,6 @@ Tab:AddButton({
 })
 
 Tab:AddButton({
-    Name = "AutoSpam",
-    Callback = function()
-      while true do
-         task.wait()
-         local ball = workspace.Balls:FindFirstChildWhichIsA("BasePart")
-         local hum = game.Players.LocalPlayer.Character
-         local balldist = 6
-         if ball then
-         local dist = (ball.Position - hum.HumanoidRootPart.position).Magnitude
-         if dist <= balldist and hum:FindFirstChild("Highlight") then
-         keypress(0x46)
-         end
-			
-Tab:AddButton({
 	Name = "Il y aura bientôt plus de choses!",
 	Callback = function()
       		print("Il y aura bientôt plus de choses!")
