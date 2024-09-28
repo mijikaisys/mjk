d87ea7e6fcf8c080d8fa11521888559839049afc
mijikaisys <mikifemboy@gmail.com>
2024-09-28 11:03:49 +0200

    Update AutoParry.lua

--- AutoParry.lua
+++ AutoParry.lua
@@ -33,7 +33,7 @@ Balls.ChildAdded:Connect(function(Ball)
             local Distance = (Ball.Position - workspace.CurrentCamera.Focus.Position).Magnitude
             local Velocity = (OldPosition - Ball.Position).Magnitude
 
-            if (Distance <= MaxDistance) and ((Distance / Velocity) <= 10) then
+            if (Distance <= MaxDistance) and ((Distance / Velocity) <= 11.5) then
     Parry()
                     end
         end
