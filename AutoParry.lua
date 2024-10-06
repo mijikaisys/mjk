82771006203882e891b16592e59f2361747d535c
mijikaisys <mikifemboy@gmail.com>
2024-10-06 03:00:26 +0200

    Update AutoParry.lua

--- AutoParry.lua
+++ AutoParry.lua
@@ -1,46 +1,51 @@
-local Players = game:GetService("Players")
-local VirtualInputManager = game:GetService("VirtualInputManager")
+-- credit to dendenzzz
 
+getgenv().autoparry = true
+
+local VirtualManager = 
+game:GetService("VirtualInputManager")
+local Stats = game:GetService('Stats')
+local Players = game:GetService('Players')
 local Player = Players.LocalPlayer or Players.PlayerAdded:Wait()
-local Balls = workspace:WaitForChild("Balls", 9e9)
+local RunService = game:GetService('RunService')
+local parry_helper = loadstring(game:HttpGet("https://raw.githubusercontent.com/TripleScript/TripleHub/main/helper_.lua"))()
 
-local function VerifyBall(Ball)
-    if typeof(Ball) == "Instance" and Ball:IsA("BasePart") and Ball:IsDescendantOf(Balls) and Ball:GetAttribute("realBall") == true then
-        return true
-    end
-end
+local ero = false
 
-local function IsTarget()
-    return (Player.Character and Player.Character:FindFirstChild("Highlight"))
-end
-
-local function Parry()
-    VirtualInputManager:SendMouseButtonEvent(0, 0, 0, true, game, 0)
-    VirtualInputManager:SendMouseButtonEvent(0, 0, 0, false, game, 0)
-end
-
-Balls.ChildAdded:Connect(function(Ball)
-    if not VerifyBall(Ball) then
-        return
-    end
-
-    local OldPosition = Ball.Position
-    local OldTick = tick()
-    local MaxDistance = 15 -- Distance maximale pour parer
-
-    Ball:GetPropertyChangedSignal("Position"):Connect(function()
-        if IsTarget() then
-            local Distance = (Ball.Position - workspace.CurrentCamera.Focus.Position).Magnitude
-            local Velocity = (OldPosition - Ball.Position).Magnitude
-
-            if (Distance <= MaxDistance) and ((Distance / Velocity) <= 12) then
-    Parry()
-                    end
+task.spawn(function()
+    RunService.PreRender:Connect(function()
+        if not getgenv().autoparry then 
+            return 
         end
 
-        if (tick() - OldTick >= 1/60) then
-            OldTick = tick()
-            OldPosition = Ball.Position
+        local par = parry_helper.FindTargetBall()
+        if not par then 
+            return 
+        end
+
+        local hat = par.AssemblyLinearVelocity
+        if par:FindFirstChild('zoomies') then 
+            hat = par.zoomies.VectorVelocity
+        end
+
+        local i = par.Position
+        local j = Player.Character.PrimaryPart.Position
+        local kil = (j - i).Unit
+        local l = Player:DistanceFromCharacter(i)
+        local m = kil:Dot(hat.Unit)
+        local n = hat.Magnitude
+
+        if m > 0 then
+            local o = l - 5
+            local p = o / n
+
+            if parry_helper.IsPlayerTarget(par) and p <= 0.50 and not ero then
+                VirtualManager:SendMouseButtonEvent(0, 0, 0, true, game, 0)
+                wait(0.01)
+                ero = true
+            end
+        else
+            ero = false
         end
     end)
 end)
