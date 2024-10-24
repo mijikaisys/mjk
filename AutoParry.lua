b2142231d014e991372e219260b568a22d3b4648
mijikaisys <mikifemboy@gmail.com>
2024-10-24 06:01:55 +0200

    Update AutoParry.lua

--- AutoParry.lua
+++ AutoParry.lua
@@ -83,7 +83,7 @@ local function initializeParry()
             local n = hat.Magnitude
 
             -- Calculer le seuil basé sur la vitesse
-            local thresholdP = 0.50 * (1 + 0.2 * velocity)
+            local thresholdP = 0.48 * (1 + 0.1 * velocity)
 
             if m > 0 then
                 local o = l - 5
