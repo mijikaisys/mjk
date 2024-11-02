e8889d96ac3a0d1468710c737b4ab2c655c5a72b
mijikaisys <mikifemboy@gmail.com>
2024-11-02 16:07:09 +0100

    Update potentiel test.lua

--- potentiel test.lua
+++ potentiel test.lua
@@ -2,47 +2,6 @@ local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
 local localPlayer = Players.LocalPlayer
 
-local a = game:GetService("VirtualInputManager")
-local b = game:GetService("RunService")
-local c = game:GetService("Players").LocalPlayer
-local d = loadstring(game:HttpGet("https://raw.githubusercontent.com/DenDenZYT/DenDenZ-s-Open-Source-Collection/main/Component"))() 
-
-local e = false
-
-spawn(function()
-    b.PreRender:Connect(function()
-        if not getgenv().f then return end
-
-        local g = d.FindTargetBall()
-        if not g then return end
-
-        local h = g.AssemblyLinearVelocity
-        if g:FindFirstChild('zoomies') then 
-            h = g.zoomies.VectorVelocity
-        end
-
-        local i = g.Position
-        local j = c.Character.PrimaryPart.Position
-        local k = (j - i).Unit
-        local l = c:DistanceFromCharacter(i)
-        local m = k:Dot(h.Unit)
-        local n = h.Magnitude
-
-        if m > 0 then
-            local o = l - 5
-            local p = o / n
-
-            if d.IsPlayerTarget(g) and p <= 0.55 and not e then
-                alertSound:Play()
-                wait(0.01)
-                e = true
-            end
-        else
-            e = false
-        end
-    end)
-end)
-
 local baseSphereRadius = 5 -- Rayon de base de la sphère de détection
 local detectionSphere
 local alertSound = Instance.new("Sound") -- Créer un son
@@ -149,3 +108,43 @@ while true do
         print("Le personnage est mort. La sphère ne sera pas mise à jour.")
     end
 end
+local a = game:GetService("VirtualInputManager")
+local b = game:GetService("RunService")
+local c = game:GetService("Players").LocalPlayer
+local d = loadstring(game:HttpGet("https://raw.githubusercontent.com/DenDenZYT/DenDenZ-s-Open-Source-Collection/main/Component"))() 
+
+local e = false
+
+spawn(function()
+    b.PreRender:Connect(function()
+        if not getgenv().f then return end
+
+        local g = d.FindTargetBall()
+        if not g then return end
+
+        local h = g.AssemblyLinearVelocity
+        if g:FindFirstChild('zoomies') then 
+            h = g.zoomies.VectorVelocity
+        end
+
+        local i = g.Position
+        local j = c.Character.PrimaryPart.Position
+        local k = (j - i).Unit
+        local l = c:DistanceFromCharacter(i)
+        local m = k:Dot(h.Unit)
+        local n = h.Magnitude
+
+        if m > 0 then
+            local o = l - 5
+            local p = o / n
+
+            if d.IsPlayerTarget(g) and p <= 0.55 and not e then
+                alertSound:Play()
+                wait(0.01)
+                e = true
+            end
+        else
+            e = false
+        end
+    end)
+end)
