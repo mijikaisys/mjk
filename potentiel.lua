cdc84ebdaedfd81a79c8c34457d66531c6a43a03
mijikaisys <mikifemboy@gmail.com>
2024-12-08 17:07:34 +0100

    Update potentiel.lua

--- potentiel.lua
+++ potentiel.lua
@@ -1,40 +1,49 @@
-local a = game:GetService("VirtualInputManager")
-local b = game:GetService("RunService")
-local c = game:GetService("Players").LocalPlayer
-local d = loadstring(game:HttpGet("https://raw.githubusercontent.com/DenDenZYT/DenDenZ-s-Open-Source-Collection/main/Component"))() 
+getgenv().autoparry = true
 
-local e = false
+local VirtualManager = 
+game:GetService("VirtualInputManager")
+local Stats = game:GetService('Stats')
+local Players = game:GetService('Players')
+local Player = Players.LocalPlayer or Players.PlayerAdded:Wait()
+local RunService = game:GetService('RunService')
+local parry_helper = loadstring(game:HttpGet("https://raw.githubusercontent.com/TripleScript/TripleHub/main/helper_.lua"))()
 
-spawn(function()
-    b.PreRender:Connect(function()
-        if not getgenv().f then return end
+local ero = false
 
-        local g = d.FindTargetBall()
-        if not g then return end
-
-        local h = g.AssemblyLinearVelocity
-        if g:FindFirstChild('zoomies') then 
-            h = g.zoomies.VectorVelocity
+task.spawn(function()
+    RunService.PreRender:Connect(function()
+        if not getgenv().autoparry then 
+            return 
         end
 
-        local i = g.Position
-        local j = c.Character.PrimaryPart.Position
-        local k = (j - i).Unit
-        local l = c:DistanceFromCharacter(i)
-        local m = k:Dot(h.Unit)
-        local n = h.Magnitude
+        local par = parry_helper.FindTargetBall()
+        if not par then 
+            return 
+        end
+
+        local hat = par.AssemblyLinearVelocity
+        if par:FindFirstChild('zoomies') then 
+            hat = par.zoomies.VectorVelocity
+        end
+
+        local i = par.Position
+        local j = Player.Character.PrimaryPart.Position
+        local kil = (j - i).Unit
+        local l = Player:DistanceFromCharacter(i)
+        local m = kil:Dot(hat.Unit)
+        local n = hat.Magnitude
 
         if m > 0 then
             local o = l - 5
             local p = o / n
 
-            if d.IsPlayerTarget(g) and p <= 0.55 and not e then
-                a:SendMouseButtonEvent(0, 0, 0, true, game, 0)
+            if parry_helper.IsPlayerTarget(par) and p <= 0.55 and not ero then
+                VirtualManager:SendMouseButtonEvent(0, 0, 0, true, game, 0)
                 wait(0.01)
-                e = true
+                ero = true
             end
         else
-            e = false
+            ero = false
         end
     end)
 end)
