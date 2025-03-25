bd444b26c21e7dc1c5975045c7c659039ba0a0d6
mijikaisys <mikifemboy@gmail.com>
2025-03-25 01:17:46 +0100

    Update Noruism.lua

--- Noruism.lua
+++ Noruism.lua
@@ -24,6 +24,7 @@ local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/
              Duration = 3
          })
          loadstring(game:HttpGet("https://raw.githubusercontent.com/mijikaisys/mjk/refs/heads/main/PreniumAutoParry.lua"))()
+   
          loadstring(game:HttpGet("https://raw.githubusercontent.com/mijikaisys/mjk/refs/heads/main/NoruismAp.lua"))()
          end
      end
